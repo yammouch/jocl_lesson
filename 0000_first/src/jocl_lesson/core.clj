bdaef5b2dbd5999146dc77d7d0493352b0351061
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-01-23 19:36:13 +0900

    A checker has been added.

--- 0000_first/src/jocl_lesson/core.clj
+++ 0000_first/src/jocl_lesson/core.clj
@@ -2,19 +2,28 @@
   (:gen-class))
 
 (require 'jocl-lesson.fft-cl)
-(alias 'fft 'jocl-lesson.fft-cl)
+(alias 'fft-cl 'jocl-lesson.fft-cl)
+
+(require 'jocl-lesson.fft)
+(alias 'fft 'jocl-lesson.fft)
 
 (require 'clojure.pprint)
 
 (defn -main [& args]
-  (dosync (ref-set fft/exp2 4))
-  (fft/init)
-  (let [n (bit-shift-left 1 @fft/exp2)
+  (dosync (ref-set fft-cl/exp2 4))
+  (fft-cl/init)
+  (let [n (bit-shift-left 1 @fft-cl/exp2)
         phase (/ (* 2 Math/PI) n)
-        swing-0db (bit-shift-left 1 14)]
-    (clojure.pprint/pprint
-     (fft/fft-mag-norm
-      (float-array (map #(Math/cos (* phase %)) 
-                        (range n)))
-      0 1.0)))
-  (fft/finalize))
+        swing-0db (bit-shift-left 1 14)
+        input (map #(Math/cos (* phase %))
+                   (range n))
+        result (vec (fft/fft-mag-norm input 1.0))
+        ;result (update-in result [0] inc)
+        result-cl (fft-cl/fft-mag-norm (float-array input) 0 1.0)]
+    (clojure.pprint/pprint result)
+    (clojure.pprint/pprint result-cl)
+    (if (every? #(< -0.01 % 0.01) (map - result result-cl))
+      (println "[OK]")
+      (println "!ER!")
+      ))
+  (fft-cl/finalize))
