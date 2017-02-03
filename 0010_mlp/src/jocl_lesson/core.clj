a97ca748e9b04e46ad5504ec46bbf01aa6120b19
Takuya Yamauchi <yammouch@gmail.com>
2017-02-04 01:03:17 +0900

    Now it gets a kernel function name.

--- 0010_mlp/src/jocl_lesson/core.clj
+++ 0010_mlp/src/jocl_lesson/core.clj
@@ -11,12 +11,15 @@
 
 (defn -main [& args]
   (mlp-cl/init)
+  (println (-> (@mlp-cl/cl-ker :add)
+               (mlp-cl/clGetKernelInfo 'CL_KERNEL_FUNCTION_NAME)
+               (cl/parse-str-info)))
   (let [{q :queue} @mlp-cl/cl-env
         {sub :sub} @mlp-cl/cl-ker
         {z :z a :a v :v w :w b :b wacc :wacc bacc :bacc
          i0 :i0 i1 :i1 i2 :i2 i3 :i3
          l0 :l0 l1 :l1 l2 :l2 l3 :l3} @mlp-cl/cl-mem]
-    (dotimes [i 500]
+    (dotimes [i 100]
       (mlp-cl/run-subbatch [i0 i1 i2 i3] [l0 l1 l2 l3])
       (when (= (mod i 10) 0)
         (let [[w] (cl/read-float q w 1)
