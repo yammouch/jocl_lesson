e07cca4cfeb41c141ee81f3c47bdf998e257ffe6
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-02-06 19:26:00 +0900

    'inputs' and 'labels' are moved to core from mlp-cl.

--- 0010_mlp/src/jocl_lesson/core.clj
+++ 0010_mlp/src/jocl_lesson/core.clj
@@ -1,6 +1,8 @@
 (ns jocl-lesson.core
   (:gen-class))
 
+(import '(org.jocl CL))
+
 (require 'jocl-lesson.cl)
 (alias 'cl 'jocl-lesson.cl)
 
@@ -9,13 +11,15 @@
 
 (defn -main [& args]
   (mlp-cl/init 3)
-  (let [{q :queue} @mlp-cl/cl-env
+  (let [{q :queue ctx :context} @mlp-cl/cl-env
         {sub "sub"} @mlp-cl/cl-ker
-        {z :z a :a v :v w :w b :b wacc :wacc bacc :bacc
-         i0 :i0 i1 :i1 i2 :i2 i3 :i3
-         l0 :l0 l1 :l1 l2 :l2 l3 :l3} @mlp-cl/cl-mem]
+        {z :z a :a v :v w :w b :b wacc :wacc bacc :bacc} @mlp-cl/cl-mem
+        inputs (map (partial cl/create-buffer ctx :f)
+                    [[0.0 ] [0.25] [0.75] [1.0 ]])
+        labels (map (partial cl/create-buffer ctx :f)
+                    (map (partial repeat 3) [0 0 1 1]))]
     (dotimes [i 100]
-      (mlp-cl/run-subbatch [i0 i1 i2 i3] [l0 l1 l2 l3])
+      (mlp-cl/run-subbatch inputs labels)
       (when (= (mod i 10) 0)
         (let [w (cl/read-float q w 3)
               b (cl/read-float q b 3)]
@@ -23,6 +27,6 @@
            i
            (apply str (interpose " " (map (partial format "%6.2f") w)))
            (apply str (interpose " " (map (partial format "%6.2f") b)))
-           (mlp-cl/fw-err-subbatch [i0 i1 i2 i3] [l0 l1 l2 l3])
-           )))))
+           (mlp-cl/fw-err-subbatch inputs labels)))))
+    (doseq [m (concat inputs labels)] (CL/clReleaseMemObject m)))
   (mlp-cl/finalize))
