421dffe3163d60fd55d85695f7cf99667d896ac8
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-02-13 19:11:45 +0900

    Long tests have been added.

--- 0050_mlp/project.clj
+++ 0050_mlp/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jocl/jocl "0.2.0-RC00"]]
+  :test-selectors {:default (complement :long-test)}
   :main ^:skip-aot mlp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
