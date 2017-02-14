9bcb5131e8b0103abb2f56c59ae3f729af2a8093
yammouch <yammouch@gmail.com>
2017-02-14 14:31:46 +0900

    An xor test has been added.

--- 0050_mlp/test/mlp/long_test.clj
+++ 0050_mlp/test/mlp/long_test.clj
@@ -90,3 +90,36 @@
         ))
     (doseq [m (concat inputs labels)] (CL/clReleaseMemObject m)))
   (mlp-cl/finalize))
+
+(deftest ^:long-test xor
+  (mlp-cl/init [2 3 3 1])
+  (let [{q :queue ctx :context} @mlp-cl/cl-env
+        {sub "sub"} @mlp-cl/cl-ker
+        {w :w b :b} @mlp-cl/cl-mem
+        inputs (map (partial cl/create-buffer ctx :f)
+                    [[0 0]
+                     [0 1]
+                     [1 0]
+                     [1 1]])
+        labels (map (partial cl/create-buffer ctx :f)
+                    [[0]
+                     [1]
+                     [1]
+                     [0]])]
+    (dotimes [i 4001]
+    ;(dotimes [i 1]
+      (mlp-cl/run-subbatch inputs labels)
+      (when (= (mod i 200) 0)
+        (printf "i: %4d err: %8.2f\n"
+         i
+         (mlp-cl/fw-err-subbatch inputs labels))
+        ;(mlp-cl/dump :w 0)
+        ;(mlp-cl/dump :b 0)
+        ;(mlp-cl/dump :w 1)
+        ;(mlp-cl/dump :b 1)
+        ;(mlp-cl/dump :w 2)
+        ;(mlp-cl/dump :b 2)
+        (flush)
+        ))
+    (doseq [m (concat inputs labels)] (CL/clReleaseMemObject m)))
+  (mlp-cl/finalize))
