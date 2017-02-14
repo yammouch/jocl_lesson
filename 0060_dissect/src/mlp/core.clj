36ccf8439eeefb35ab743c76a705ff81bbb95d25
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-02-14 14:39:55 +0900

    core.clj has been refactored somehow.

--- 0060_dissect/src/mlp/core.clj
+++ 0060_dissect/src/mlp/core.clj
@@ -9,33 +9,37 @@
 (require 'mlp.mlp-cl)
 (alias 'mlp-cl 'mlp.mlp-cl)
 
+(def inputs-v
+  [[0 0 0]
+   [0 0 1]
+   [0 1 0]
+   [0 1 1]
+   [1 0 0]
+   [1 0 1]
+   [1 1 0]
+   [1 1 1]])
+
+(def labels-v
+  [[0 0 0 0 1]
+   [0 0 1 0 1]
+   [0 1 0 0 1]
+   [0 1 1 1 1]
+   [1 0 0 0 1]
+   [1 0 1 1 1]
+   [1 1 0 1 1]
+   [1 1 1 1 0]])
+
 (defn -main [& args]
   (mlp-cl/init [3 4 5])
   (let [{q :queue ctx :context} @mlp-cl/cl-env
         {sub "sub"} @mlp-cl/cl-ker
         {w :w b :b} @mlp-cl/cl-mem
-        inputs (map (partial cl/create-buffer ctx :f)
-                    [[0 0 0]
-                     [0 0 1]
-                     [0 1 0]
-                     [0 1 1]
-                     [1 0 0]
-                     [1 0 1]
-                     [1 1 0]
-                     [1 1 1]])
-        labels (map (partial cl/create-buffer ctx :f)
-                    [[0 0 0 0 1]
-                     [0 0 1 0 1]
-                     [0 1 0 0 1]
-                     [0 1 1 1 1]
-                     [1 0 0 0 1]
-                     [1 0 1 1 1]
-                     [1 1 0 1 1]
-                     [1 1 1 1 0]])]
-    (dotimes [i 5001]
+        inputs (map (partial cl/create-buffer ctx :f) inputs-v)
+        labels (map (partial cl/create-buffer ctx :f) labels-v)]
+    (dotimes [i 1001]
     ;(dotimes [i 1]
       (mlp-cl/run-subbatch inputs labels)
-      (when (= (mod i 200) 0)
+      (when (= (mod i 50) 0)
         (printf "i: %4d err: %8.2f\n"
          i
          (mlp-cl/fw-err-subbatch inputs labels))
