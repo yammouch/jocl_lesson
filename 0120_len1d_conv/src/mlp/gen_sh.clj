c2d06b364a07beeab930c81539fef3835cd9c994
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-03-14 20:01:01 +0900

    Batch is ready.

--- 0120_len1d_conv/src/mlp/gen_sh.clj
+++ 0120_len1d_conv/src/mlp/gen_sh.clj
@@ -3,13 +3,37 @@
 (ns mlp.gen-sh
   (:gen-class))
 
+(defn print1 []
+  (doseq [[conv-size conv-depth] (for [s [2 5 10 20] d [2 5 10 20]] [s d])]
+    (printf (str "lein run  10 10 100001 0.1 1 %2d %2d | "
+                 "tee result/_10_10_%s_%s.log\n")
+            conv-size conv-depth
+            (apply str (map #(if (= % \space) \_ %)
+                            (format "%2d" conv-size)))
+            (apply str (map #(if (= % \space) \_ %)
+                            (format "%2d" conv-depth))))))
+
+(defn print2 []
+  (doseq [[conv-size conv-depth] (for [s [5 10 20 40] d [5 10 20 40]] [s d])]
+    (printf (str "lein run  20 10 100001 0.1 1 %2d %2d | "
+                 "tee result/_20_10_%s_%s.log\n")
+            conv-size conv-depth
+            (apply str (map #(if (= % \space) \_ %)
+                            (format "%2d" conv-size)))
+            (apply str (map #(if (= % \space) \_ %)
+                            (format "%2d" conv-depth))))))
+
+(defn print3 []
+  (doseq [[conv-size conv-depth] (for [s [10 20 40 80] d [10 20 40 64]] [s d])]
+    (printf (str "lein run 100 20 100001 0.1 1 %2d %2d | "
+                 "tee result/100_20_%s_%s.log\n")
+            conv-size conv-depth
+            (apply str (map #(if (= % \space) \_ %)
+                            (format "%2d" conv-size)))
+            (apply str (map #(if (= % \space) \_ %)
+                            (format "%2d" conv-depth))))))
+
 (defn -main [& _]
-  (dorun
-    (for [l1 [3 4 5 6 8 10 12 15 20 30]
-          l2 [3 4 5 6 8 10 12 15 20 30]]
-      (printf "lein run 10 10 100001 0.1 1 %2d %2d | tee result/%s_%s.log\n"
-              l1 l2
-              (apply str (map #(if (= % \space) \_ %)
-                              (format "%2d" l1)))
-              (apply str (map #(if (= % \space) \_ %)
-                              (format "%2d" l2)))))))
+  (print1)
+  (print2)
+  (print3))
