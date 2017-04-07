ef66336b397d8e05ea4a7d8e4bd3d28421b65740
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-04-07 11:00:34 +0900

    It works well.

---
+++ 0190_jk/shells/ex01.sh
@@ -0,0 +1,12 @@
+host=wup35031
+
+ssh $host mkdir /tmp/tyam
+dt=`ssh $host date "+%Y%m%d_%H%M%S"`
+dir="/tmp/tyam/0190_jk_len2d_$dt"
+ssh $host mkdir $dir
+ssh $host mkdir $dir/result
+scp ../target/uberjar/mlp-0.1.0-SNAPSHOT-standalone.jar $host:$dir/run.jar
+cat <<EOS | ssh $host "cat > $dir/run.sh"
+java -cp run.jar mlp.core 5 5 1001 0.1 1 5 5 > result/__5__5__5__5.log
+EOS
+ssh $host "cd $dir; at -f run.sh 4:00"
