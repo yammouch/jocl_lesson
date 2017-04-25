3cfb16eabb7eb6e044526c12fab4db06783064ce
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-04-25 12:13:05 +0900

    'slide-upper' has been updated.

--- 0190_jk/src/mlp/schemanip.clj
+++ 0190_jk/src/mlp/schemanip.clj
@@ -2,25 +2,28 @@
 
 ; {:cmd :move-x :org [4 6] :dst 8}
 
-(defn slide-upper-field [field]
+(defn slide-upper-field
+ ([field] (slide-upper-field field 0))
+ ([field empty]
   (assoc field :body
-         (concat (next (get field :body))
-                 [(reduce #(repeat %2 %1) 0
-                          [(count (get-in field [:body 0 0]))
-                           (get-in field [:size 0])])])))
+         (concat (next (field :body))
+                 [(repeat (count (nth (field :body) 0)) empty)]
+                 ))))
 
-(defn slide-upper [{field :field cmd :cmd :as x}]
+(defn slide-upper
+ ([x] (slide-upper x 0))
+ ([{field :field cmd :cmd :as x} empty]
   (if (or (= (get-in cmd [:org 1]) 0)
           (and (= (:cmd cmd) :move-y)
                (= (:dst cmd) 0))
-          (some (partial some (complement zero?))
+          (some (partial not= empty)
                 (first (:body field))))
     nil
     (-> (if (= (:cmd cmd) :move-y)
           (update-in x [:cmd :dst] dec)
           x)
-        (update-in [:field] slide-upper-field)
-        (update-in [:cmd :org 1] dec))))
+        (update-in [:field] #(slide-upper-field % empty))
+        (update-in [:cmd :org 1] dec)))))
 
 (defn slide-lower-field [field]
   (assoc field :body
