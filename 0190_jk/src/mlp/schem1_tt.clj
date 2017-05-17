70fd4927ec940270add2f2eece93150786509d6a
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-05-17 11:55:36 +0900

    Now unduplicated random values are generated from 'rand-nodup' function.

---
+++ 0190_jk/src/mlp/schem1_tt.clj
@@ -0,0 +1,14 @@
+(ns mlp.schem1-tt)
+
+(defn lift [[x & xs] n]
+  (cond (not x) n
+        (< n x) n
+        :else (recur xs (+ 1 n))
+        ))
+
+(defn rand-nodup [n lt rs]
+  (loop [acc (sorted-set)
+         [x & xs] (map rem rs (range lt (- lt n) -1))]
+    (if x
+      (recur (conj acc (lift (seq acc) x)) xs)
+      acc)))
