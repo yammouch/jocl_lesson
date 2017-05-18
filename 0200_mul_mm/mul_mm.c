c72c63316f627277f362ffa7a909d24eea13fd75
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-05-18 19:53:40 +0900

    C program supports height and width of the product.

--- 0200_mul_mm/mul_mm.c
+++ 0200_mul_mm/mul_mm.c
@@ -9,12 +9,13 @@ void init_array(int n, float *x) {
   }
 }
 
-void mul_mm(float *om, const float *m0, const float *m1, int c_div_32) {
+void mul_mm(float *om, const float *m0, const float *m1,
+            int h_div_32, int c_div_32, int w_div_32) {
   int i, j, k;
-  for (i = 0; i < 32; i++) {
+  for (i = 0; i < 32*h_div_32; i++) {
     for (k = 0; k < 32*c_div_32; k++) {
-      for (j = 0; j < 32; j++) {
-        om[i*32+j] += m0[i*c_div_32*32+k] * m1[k*32+j];
+      for (j = 0; j < 32*w_div_32; j++) {
+        om[i*32*w_div_32+j] += m0[i*c_div_32*32+k] * m1[k*32*w_div_32+j];
       }
     }
   }
@@ -25,20 +26,22 @@ void mul_mm(float *om, const float *m0, const float *m1, int c_div_32) {
 int main(int argc, char *argv[]) {
   float *om, *m0, *m1;
   clock_t elapsed;
-  int c_div_32;
+  int h_div_32, c_div_32, w_div_32;
 
-  c_div_32 = atoi(argv[1]);
+  h_div_32 = atoi(argv[1]);
+  c_div_32 = atoi(argv[2]);
+  w_div_32 = atoi(argv[3]);
 
-  om = (float *)malloc(32*32*sizeof(float));
-  m0 = (float *)malloc(32*32*c_div_32*sizeof(float));
-  m1 = (float *)malloc(32*32*c_div_32*sizeof(float));
+  om = (float *)malloc(32*h_div_32*32*w_div_32*sizeof(float));
+  m0 = (float *)malloc(32*h_div_32*32*c_div_32*sizeof(float));
+  m1 = (float *)malloc(32*c_div_32*32*w_div_32*sizeof(float));
 
-  init_array(32*32, om);
-  init_array(32*32*c_div_32, m0);
-  init_array(32*32*c_div_32, m1);
+  init_array(32*h_div_32*32*w_div_32, om);
+  init_array(32*h_div_32*32*c_div_32, m0);
+  init_array(32*c_div_32*32*w_div_32, m1);
 
   elapsed = clock();
-  mul_mm(om, m0, m1, c_div_32);
+  mul_mm(om, m0, m1, h_div_32, c_div_32, w_div_32);
   elapsed = clock() - elapsed;
   printf("It took %.6f seconds.\n", (double)elapsed/CLOCKS_PER_SEC);
 
