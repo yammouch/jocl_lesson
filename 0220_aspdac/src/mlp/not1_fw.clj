b30c8b4aec211ca81a291ed556658f2a51d6b7a0
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-06-12 17:49:13 +0900

    Refactored.

--- 0220_aspdac/src/mlp/not1_fw.clj
+++ 0220_aspdac/src/mlp/not1_fw.clj
@@ -1,6 +1,7 @@
 (ns mlp.not1-fw
   (:gen-class)
-  (:require [mlp.mlp-jk :as mlp]))
+  (:require [mlp.mlp-jk :as mlp]
+            [clojure.pprint]))
 
 (defn radix [x]
   (loop [x x acc []]
@@ -9,18 +10,14 @@
       (recur (quot x 2) (conj acc (rem x 2)))
       )))
 
-(defn mlp-input-field [{body :body}]
+(defn mlp-input-field [body]
   (mapcat #(take 5 (concat (radix %) (repeat 0)))
           (apply concat body)))
 
 (def schems
   (->> (read-string (str "(" (slurp "src/mlp/not1.dat") ")"))
        (partition 3)
-       (map (fn [[_ field cmd]]
-              {:field {:body (mapv (fn [row] (mapv #(Integer/parseInt % 16)
-                                                   (re-seq #"\S+" row)))
-                                   field)}
-               :cmd cmd}))))
+       (map second)))
 
 (defn read-param []
   (let [[x & xs] (read-string (str "(" (slurp "param.dat") ")"))]
@@ -32,14 +29,22 @@
       (alter mlp/jk-mem #(assoc-in % [i :p] (float-array p)))
       )))
 
-(defn -main [& args]
-  (let [[mlp-config params] (read-param)]
-    (mlp/init mlp-config 0)
-    (set-param params)
-    (mlp/fw (float-array (mlp-input-field (:field (nth schems 4)))))
+(defn fw [schem]
+  (clojure.pprint/pprint schem)
+  (let [parsed (mapv (fn [row] (mapv #(Integer/parseInt % 16)
+                                     (re-seq #"\S+" row)))
+                     schem)]
+    (mlp/fw (float-array (mlp-input-field parsed)))
     (loop [[x & xs] [2 10 10 10], l (:i (last @mlp/jk-mem))]
       (when x
         (println (apply str (interpose " " (map (partial format "%3.1f")
                                                 (take x l)))))
         (recur xs (drop x l))
         ))))
+
+(defn -main [& args]
+  (let [[mlp-config params] (read-param)]
+    (mlp/init mlp-config 0)
+    (set-param params)
+    (fw (nth schems 4))
+    ))
