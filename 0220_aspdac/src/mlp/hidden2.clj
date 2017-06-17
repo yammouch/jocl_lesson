35f4e1602b3678f616aafb610e0e4b41379b72b0
Tackya Yammouch <yammouch@gmail.com>
2017-06-17 17:17:57 +0900

    Updated.

--- 0220_aspdac/src/mlp/hidden2.clj
+++ 0220_aspdac/src/mlp/hidden2.clj
@@ -1,5 +1,7 @@
 (ns mlp.hidden2
-  (:gen-class))
+  (:gen-class)
+  (:require [mlp.mlp-clj :as mlp]
+            [clojure.pprint]))
 
 (defn read-file []
   (->> (slurp "data/p001.dat")
@@ -7,7 +9,10 @@
        read-string
        (#(nth % 4))))
 
-(defn -main [& args]
-  (->> (read-file)
-       (take 4)
-       println))
+(defn -main [& _]
+  (let [layer2 (reduce #(partition %2 %1) (read-file) [4 4 3])]
+    (clojure.pprint/pprint layer2)
+    (-> [[[1.0 0.0 0.0 0.0]]]
+        (mlp/padding 2 2 2 2)
+        (mlp/conv-fw (reverse (map reverse layer2)) true)
+        clojure.pprint/pprint)))
