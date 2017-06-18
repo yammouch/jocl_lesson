614c4215232315265cf3afbc6be82cb1c438a64c
Tackya Yammouch <yammouch@gmail.com>
2017-06-18 11:20:19 +0900

    Updated.

--- 0220_aspdac/src/mlp/hidden2.clj
+++ 0220_aspdac/src/mlp/hidden2.clj
@@ -3,6 +3,15 @@
   (:require [mlp.mlp-clj :as mlp]
             [clojure.pprint]))
 
+(defn rmap
+ ([f tr] (rmap f tr (comp not seq?)))
+ ([f tr atom?]
+  (cond (empty? tr) '()
+        (atom? tr) (f tr)
+        :else (cons (rmap f (first tr))
+                    (rmap f (rest  tr))
+                    ))))
+
 (defn read-file []
   (let [read-data (->> (slurp "data/p001.dat")
                        (#(str "[" % "]"))
@@ -10,9 +19,13 @@
     [(reduce #(partition %2 %1) (nth read-data 2) [4 5 3])
      (reduce #(partition %2 %1) (nth read-data 4) [4 4 3])]))
 
-;(defn show-l01 [l]
-;  (let [bunches (->> l
-;                     (
+(defn show-l01 [l]
+  (let [d0 (rmap #(nth % 0) l #(and (seq? %) (not (seq? (first %)))))]
+    (doseq [r d0]
+      (->> r
+           (map (partial format "%4.1f"))
+           (apply println)
+           ))))
 
 (defn -main [& _]
   (let [[l1 l2] (read-file)
@@ -24,4 +37,5 @@
                 (mlp/conv-fw (reverse (map reverse l1)) true))]
     (clojure.pprint/pprint l2)
     (clojure.pprint/pprint l12)
-    (clojure.pprint/pprint l01)))
+    (clojure.pprint/pprint l01)
+    (show-l01 l01)))
