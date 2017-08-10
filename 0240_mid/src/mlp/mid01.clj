d99ce68d9af24462e13b2f7d772359c7c919c9a6
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2017-08-10 19:20:47 +0900

    Input data has been replaced to all 0.

--- 0240_mid/src/mlp/mid01.clj
+++ 0240_mid/src/mlp/mid01.clj
@@ -1,5 +1,5 @@
 ; - (done) Reduce layers.
-; - Replace input data.
+; - (done) Replace input data.
 ; - Add error calculation.
 ; - Calculate gradient of input vector.
 ; - Add updating of input vector.
@@ -10,23 +10,6 @@
   (:gen-class)
   (:require [mlp.mlp-jk :as mlp]
             [clojure.pprint]))
-
-(defn radix [x]
-  (loop [x x acc []]
-    (if (<= x 0)
-      acc
-      (recur (quot x 2) (conj acc (rem x 2)))
-      )))
-
-(defn mlp-input-field [body]
-  (mapcat #(take 6 (concat (radix %) (repeat 0)))
-          (apply concat body)))
-
-(defn read-schem [fname num]
-  (->> (read-string (str "(" (slurp fname) ")"))
-       (partition 3)
-       (filter (comp #{num} first))
-       (map second)))
 
 (defn read-param [fname]
   (let [[x & xs] (read-string (str "(" (slurp fname) ")"))]
@@ -49,12 +32,8 @@
          (apply str)
          println)))
 
-(defn fw [schem]
-  (clojure.pprint/pprint schem)
-  (let [parsed (mapv (fn [row] (mapv #(Integer/parseInt % 16)
-                                     (re-seq #"\S+" row)))
-                     schem)
-        nn-input (float-array (mlp-input-field parsed))]
+(defn fw []
+  (let [nn-input (float-array (* 10 10 6))]
     (mlp/fw nn-input)
     (->> nn-input
          (partition 6)
@@ -72,9 +51,6 @@
 
 (defn -main []
   (let [param-fname "data/0_2_4_6_7_8_9_10_11_12_13_15_18_20_22_23_25_26_28_29_30_32_33_35.prm"
-        schem-fname "data/not1.dat"
-        schem-num "13"
-        schems (read-schem schem-fname (read-string schem-num))
         [_ params] (read-param param-fname)]
     (mlp/init
      [{:type :conv, :size [3 3 4], :isize [10 10 6], :pad [1 1 1 1]}
@@ -82,5 +58,4 @@
       {:type :cross-entropy, :size [400]}]
      0)
     (set-param params)
-    (doseq [s schems] (fw s))
-    ))
+    (fw)))
