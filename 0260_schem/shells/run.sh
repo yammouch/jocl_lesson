8b1a7e097f38230dc02b4410391d9dbcd4e1074e
yammouch <yammouch@gmail.com>
2018-01-16 12:45:19 +0900

    First checkin.

---
+++ 0260_schem/shells/run.sh
@@ -0,0 +1,11 @@
+lein run -m mlp.not1-large 200000 10 10 data/not1.dat data/hoge.dat 0 | tee data/10.log
+lein run -m mlp.not1-large 200000 11 11 data/not1.dat data/hoge.dat 0 | tee data/11.log
+lein run -m mlp.not1-large 200000 12 12 data/not1.dat data/hoge.dat 0 | tee data/12.log
+lein run -m mlp.not1-large 200000 13 13 data/not1.dat data/hoge.dat 0 | tee data/13.log
+lein run -m mlp.not1-large 200000 14 14 data/not1.dat data/hoge.dat 0 | tee data/14.log
+lein run -m mlp.not1-large 200000 15 15 data/not1.dat data/hoge.dat 0 | tee data/15.log
+lein run -m mlp.not1-large 200000 16 16 data/not1.dat data/hoge.dat 0 | tee data/16.log
+lein run -m mlp.not1-large 200000 17 17 data/not1.dat data/hoge.dat 0 | tee data/17.log
+lein run -m mlp.not1-large 200000 18 18 data/not1.dat data/hoge.dat 0 | tee data/18.log
+lein run -m mlp.not1-large 200000 19 19 data/not1.dat data/hoge.dat 0 | tee data/19.log
+lein run -m mlp.not1-large 200000 20 20 data/not1.dat data/hoge.dat 0 | tee data/20.log
