6e05e09e2533306d27ae615b80dba91d89afc077
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2018-02-05 18:42:52 +0900

    'slide' has been added.

--- 0270_sasimi/src/mlp/schemprep.clj
+++ 0270_sasimi/src/mlp/schemprep.clj
@@ -1,5 +1,10 @@
 (ns mlp.schemprep)
 
+(defn mapd [f d l & ls]
+  (if (<= d 0)
+    (apply f l ls)
+    (apply mapv (partial mapd f (- d 1)) l ls)))
+
 (defn count-empty-row-up [field]
   (as-> field fld
         (take-while (partial every? (partial every? zero?)) fld)
@@ -10,3 +15,19 @@
         l (apply map vector field)
         r (reverse l)]
     (map count-empty-row-up [field b l r])))
+
+(defn slide [field n o]
+  (let [empty (as-> field x
+                    (iterate first x)
+                    (take-while seq? x)
+                    (map count x)
+                    (drop (+ o 1) x)
+                    (reduce #(vec (repeat %2 %1)) 0 x))
+        fslide (if (< n 0)
+                 (fn [l] (vec (take (count l)
+                                    (concat (drop (- n) l)
+                                            (repeat empty)))))
+                 (fn [l] (vec (take (count l)
+                                    (concat (repeat n empty)
+                                            l)))))]
+    (mapd fslide o field)))
